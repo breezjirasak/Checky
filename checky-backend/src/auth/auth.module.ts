576a8c5138ce793aef24b21a7f8a8e103fae4e75
breezjirasak <jirasak.tu@ku.th>
2024-06-09 20:19:20 +0700

    provide jwt for authen

--- checky-backend/src/auth/auth.module.ts
+++ checky-backend/src/auth/auth.module.ts
@@ -6,6 +6,7 @@ import { User, UserSchema } from './schema/user.schema';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { ConfigService } from '@nestjs/config';
     ]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy],
+  exports: [JwtStrategy, PassportModule]
 })
 export class AuthModule {}
